24c53d33258ea0b15f2d85cb46a2213e07307245
Nathan N <Nitharios@users.noreply.github.com>
2017-10-06 16:04:12 -1000

    Basic route added

--- server.js
+++ server.js
@@ -6,7 +6,33 @@ let bodyParser = require('body-parser');
 let express = require('express');
 let app = express();
 
+let buzzWords = {
+  "buzzWords" : []
+};
+
 app.use(express.static('public'));
+
+app.get('/buzzwords', (req, res) => {
+  res.send(buzzWords);
+});
+
+app.post('/buzzword', (req, res) => {
+
+});
+
+app.post('/reset', (req, res) => {
+
+});
+
+app.put('/buzzword', (req, res) => {
+
+});
+
+app.delete('/buzzword', (req, res) => {
+  buzzWords = {
+    "buzzWords" : []
+  };
+});
 
 app.get('/', (req, res) => {
   res. send('Hello World!');
