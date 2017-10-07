001a8f3f41afbfc2e23079b4408ee0540f10a41b
Nathan N <Nitharios@users.noreply.github.com>
2017-10-07 10:47:29 -1000

    Fixed bugs

--- server.js
+++ server.js
@@ -15,23 +15,23 @@ let index, returnValue;
 
 const app = express();
 // setting value to false forces the value in a key-value pair to be a string or an array
-app.use(bodyParser.urlencoded({ "extended" : false }));
+app.use(bodyParser.urlencoded({ "extended" : true }));
 // combines req.url with the provided root directory 'public'
 // automatically defaults to index.html
 app.use(express.static('public'));
 app.listen(PORT);
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  let err = new Error('Not Found!');
-  err.status = 404;
-  next(err);
-}); 
+// // catch 404 and forward to error handler
+// app.use((req, res, next) => {
+//   let err = new Error('Not Found!');
+//   err.status = 404;
+//   next(err);
+// }); 
 
 app.post('/reset', (req, res) => {
-  buzzWords = [];
+  buzzWordArray = [];
 
-  console.log(buzzWords);
+  console.log(buzzWordArray);
   // .json is identical to .send but will convert non-objects such as null and undefined, which are invalid as JSON
   // can take advantage of json replacer and json spaces to format JSON
   res.json(validAction);
@@ -48,13 +48,18 @@ app.route('/buzzword')
   .post((req, res) => {
     returnValue = helpers.buzzWordChecker(req, buzzWordArray);
 
-    if (returnValue !== -1) {
+    if (buzzWordArray.length >= 5) {
       res.json(invalidAction);
-
+    
     } else {
-      buzzWordArray.push(req.body);
-      console.log(buzzWordArray);
-      res.json(validAction);
+      if (returnValue !== -1) {
+        res.json(invalidAction);
+
+      } else {
+        buzzWordArray.push(req.body);
+        console.log(buzzWordArray);
+        res.json(validAction);
+      }      
     }
   })
 
