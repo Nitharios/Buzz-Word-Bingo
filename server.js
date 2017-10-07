97c9f70e799de359daa1a61282eb5ef444702b89
Nathan N <Nitharios@users.noreply.github.com>
2017-10-06 18:54:48 -1000

    Cleaned up code

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ console.log(sanity);
 const PORT = process.env.PORT || 7777;
 const bodyParser = require('body-parser');
 const express = require('express');
-// const helpers = require('./helpers');
+const helpers = require('./helpers');
 
 const app = express();
 
@@ -23,51 +23,49 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 }).listen(PORT);
 
-app.get('/buzzwords', (req, res) => {
-  res.json({
-    'buzzWord' : buzzWordArray
-  });
-});
-
-app.post('/buzzword', (req, res) => {
-  buzzWordArray.push(req.body);
-  console.log(buzzWordArray);
- 
-  res.send(validAction);
-});
-
-app.put('/buzzword', (req, res) => {
-  for (let i = 0; i < buzzWordArray.length; i++) {
-    if (buzzWordArray[i].buzzWord === req.body.buzzWord) {
-      buzzWordArray[i].heard = true;
-      score += Number(buzzWordArray[i].points);
-
-      console.log(buzzWordArray);
-      res.send({
-        "success" : true,
-        newScore : score
-      });
-    }
-  }
-  res.send(invalidAction);
-});
-
-app.delete('/buzzword', (req, res) => {
-  for (let i = 0; i < buzzWordArray.length; i++) {
-    if (buzzWordArray[i].buzzWord === req.body.buzzWord) {
-      buzzWordArray.splice(i, 1);
-      console.log(buzzWordArray);
-
-      res.send(validAction);
-    }
-  }  
-  
-  res.send(invalidAction);
-});
-
 app.post('/reset', (req, res) => {
   buzzWords = [];
 
   console.log(buzzWords);
   res.send(validAction);
 });
+
+app.get('/buzzwords', (req, res) => {
+  res.json({
+    'buzzWord' : buzzWordArray
+  });
+});
+
+app.route('/buzzword')
+  .post((req, res) => {
+    buzzWordArray.push(req.body);
+    console.log(buzzWordArray);
+   
+    res.send(validAction);
+  })
+  .put((req, res) => {
+    for (let i = 0; i < buzzWordArray.length; i++) {
+      if (buzzWordArray[i].buzzWord === req.body.buzzWord) {
+        buzzWordArray[i].heard = true;
+        score += Number(buzzWordArray[i].points);
+
+        console.log(buzzWordArray);
+        return res.send({
+          "success" : true,
+          newScore : score
+        });
+      }
+    }
+    res.send(invalidAction);
+  })
+  .delete((req, res) => {
+    for (let i = 0; i < buzzWordArray.length; i++) {
+      if (buzzWordArray[i].buzzWord === req.body.buzzWord) {
+        buzzWordArray.splice(i, 1);
+        
+        console.log(buzzWordArray);
+        return res.send(validAction);
+      }
+    }    
+    res.send(invalidAction);
+  });
